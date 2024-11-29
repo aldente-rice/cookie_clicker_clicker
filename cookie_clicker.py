eaeecb3eba7e329c598fdb8ec4b46015651f0f64
bryan <bryan04c@gmail.com>
2024-11-29 16:18:02 -0500

    Completed: Click on the upgrades when available.
    Next: make code run until the user's cookie target has been reached, rather than a hardcoded amount

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -24,15 +24,59 @@ WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.ID, "bigCookie"))
 )
 
+time.sleep(2)  # wait for cookie to load to start clicking
 cookie = driver.find_element(By.ID, "bigCookie")
 
-time.sleep(1)  # wait for it to load to start clicking
-get_cookie_score = driver.find_element(By.ID, "cookies").text.split()
-cookie_score = int(get_cookie_score[0])
-while cookie_score < 10000:
+i = 0
+while i < 100000:
     cookie.click()
-    cookie_score += 1
+
+    num_of_upgrades = 20
+    for i in range(0, num_of_upgrades):  # looks through available upgrades, upgrades if enough cookies
+
+        upgrade = driver.find_element(By.ID, f"product{i}")
+        test_var_1 = driver.find_element(By.ID, f"productPrice{i}").text
+        upgrade_price = int(driver.find_element(By.ID, f"productPrice{i}").text.replace(",", ""))
+
+        try:
+            match upgrade.get_attribute("class"):  # look to see if upgrade is available or not, using class name
+                case "product locked disabled toggledOff" | "product locked disabled":
+                    break  # skips the upgrades since they are locked or can't afford
+                case "product unlocked enabled":
+                    upgrade.click()
+        except AttributeError:
+            break
+
+        i += 1
+
+    # get_cookie_score = driver.find_element(By.ID, "cookies").text.split()
+    # cookie_score = int(get_cookie_score[0])
+
+    # cookie_score += 1
+    # num_of_upgrades = 20
+    # for i in range(0, num_of_upgrades):  # looks through available upgrades, upgrades if enough cookies
+    #     upgrade = driver.find_element(By.ID, f"product{i}")
+    #     test_var_1 = driver.find_element(By.ID, f"productPrice{i}").text
+    #     print(test_var_1 + "1")
+    #     upgrade_price = int(driver.find_element(By.ID, f"productPrice{i}").text)
+    #
+    #     match upgrade.__getattribute__("class"):  # look to see if upgrade is available or not, using class name
+    #         case "product locked disabled toggledOff":
+    #             break
+    #         case "product locked disabled":
+    #             continue
+    #         case "product unlocked enabled":
+    #             upgrade.click()
+    #
+    #     # get_cookie_score = driver.find_element(By.ID, "cookies").text.split()
+    #     #
+    #     # if upgrade_price <= cookie_score:
+    #     #     upgrade.click()
+    #     i += 1
+
+    i += 1
 
 
 time.sleep(5)
 driver.quit()
+
